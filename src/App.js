e60a0eb13d1b6d398dd6a885a87f704527a8db1d
Younji Woo <younjiwoo@gmail.com>
2023-09-14 16:08:17 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<h1>Project 3: Grocery List</h1>
+			<div className="shopping-list">
+				<h2>Groceries to buy</h2>
+				<form>
+					<input
+						type="text"
+						name="item"
+						placeholder="Add a new item"
+						required
+					/>
+					<button>Add</button>
+				</form>
+				<ul></ul>
+			</div>
+		</>
+	);
 }
 
 export default App;
