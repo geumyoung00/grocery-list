feb4a9d9385285533c6a0939c1245b8627804f7a
geumyoung <1gyn093@gmail.com>
2023-09-15 00:20:59 +0900

    항목추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import './styles.css';
+import React, { useState } from "react";
+import "./styles.css";
 
 function App() {
-	return (
-		<>
-			<h1>Project 3: Grocery List</h1>
-			<div className="shopping-list">
-				<h2>Groceries to buy</h2>
-				<form>
-					<input
-						type="text"
-						name="item"
-						placeholder="Add a new item"
-						required
-					/>
-					<button>Add</button>
-				</form>
-				<ul></ul>
-			</div>
-		</>
-	);
+  const [items, setItems] = useState([]);
+  const [addItem, setAddItem] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setItems((prev) => [...prev, addItem]);
+    e.target.reset();
+  };
+
+  const inputHandler = (e) => {
+    setAddItem(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>Project 3: Grocery List</h1>
+      <div className="shopping-list">
+        <h2>Groceries to buy</h2>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            name="item"
+            placeholder="Add a new item"
+            required
+            onChange={inputHandler}
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          {items.map((el, idx) => (
+            <li key={idx}>
+              {el}
+              <button className="delete">x</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
